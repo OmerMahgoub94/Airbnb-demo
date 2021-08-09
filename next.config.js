e14e05c48f6e94d1e9ae9d9d7a991ff1293b94a3
unknown <omer.mhjoub@gmail.com>
2021-08-09 14:13:29 +0300

    remove env

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     images: {
         domains: ['links.papareact.com']
     },
-    env: {
-        mapboxKey: 'pk.eyJ1IjoieWFiYW5pIiwiYSI6ImNrczMxN3hjODFjcmMydnBqcnlsejdsaDUifQ.cFXnmN521Hn_ZRW31TCuaA'
-    }
+    // env: {
+    //     // mapboxKey: 'pk.eyJ1IjoieWFiYW5pIiwiYSI6ImNrczMxN3hjODFjcmMydnBqcnlsejdsaDUifQ.cFXnmN521Hn_ZRW31TCuaA'
+    // }
 };
